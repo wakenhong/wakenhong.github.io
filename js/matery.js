23a296b8667d554940f1ac7cc62334ace0bcd81d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-10 16:34:49 +0800

    Deploy wakenhong/wakenhong.github.io to github.com/wakenhong/wakenhong.github.io.git:gh-pages

--- js/matery.js
+++ js/matery.js
@@ -23,15 +23,6 @@ $(function () {
         }
 
         let w = srcDiv.width();
-        if (w >= 450) {
-            w = w + 21;
-        } else if (w >= 350 && w < 450) {
-            w = w + 18;
-        } else if (w >= 300 && w < 350) {
-            w = w + 16;
-        } else {
-            w = w + 14;
-        }
         $('#' + targetId).width(w);
     };
 
@@ -46,7 +37,7 @@ $(function () {
      * 修复样式.
      */
     let fixStyles = function () {
-        fixPostCardWidth('navContainer');
+        fixPostCardWidth('navContainer', 'articles');
         fixPostCardWidth('artDetail', 'prenext-posts');
         fixFooterPosition();
     };
@@ -57,10 +48,10 @@ $(function () {
         fixStyles();
     });
 
-    /*初始化瀑布流布局*/
-    $('#articles').masonry({
-        itemSelector: '.article'
-    });
+    // /*初始化瀑布流布局*/
+    // $('#articles').masonry({
+    //     itemSelector: '.article'
+    // });
 
     AOS.init({
         easing: 'ease-in-out-sine',
@@ -105,9 +96,7 @@ $(function () {
             // 启用字幕
             subHtmlSelectorRelative: true
         });
-        $(document).find('img[data-original]').each(function(){
-            $(this).parent().attr("href", $(this).attr("data-original"));
-        });
+
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
@@ -117,7 +106,10 @@ $(function () {
         }
     };
     articleInit();
-
+    
+    $(document).find('img[data-original]').each(function(){
+        $(this).parent().attr("href", $(this).attr("data-original"));
+    });
     $('.modal').modal();
 
     /*回到顶部*/
@@ -129,46 +121,15 @@ $(function () {
     /*监听滚动条位置*/
     let $nav = $('#headNav');
     let $backTop = $('.top-scroll');
-    // 当页面处于文章中部的时候刷新页面，因为此时无滚动，所以需要判断位置,给导航加上绿色。
-    showOrHideNavBg($(window).scrollTop());
     $(window).scroll(function () {
         /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
         let scroll = $(window).scrollTop();
-        showOrHideNavBg(scroll);
-    });
-
-    function showOrHideNavBg(position) {
-        let showPosition = 100;
-        if (position < showPosition) {
+        if (scroll < 100) {
             $nav.addClass('nav-transparent');
             $backTop.slideUp(300);
         } else {
             $nav.removeClass('nav-transparent');
             $backTop.slideDown(300);
         }
-    }
-
-    	
-	$(".nav-menu>li").hover(function(){
-		$(this).children('ul').stop(true,true).show();
-		 $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
-		
-	},function(){
-		$(this).children('ul').stop(true,true).hide();
-		$('.nav-item.nav-show').removeClass('nav-show');
-	})
-	
-    $('.m-nav-item>a').on('click',function(){
-            if ($(this).next('ul').css('display') == "none") {
-                $('.m-nav-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(100);
-                $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
-            }else{
-                $(this).next('ul').slideUp(100);
-                $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
-            }
     });
-
-    // 初始化加载 tooltipped.
-    $('.tooltipped').tooltip();
 });
